e12b175d5db4d167b234253345a6053a974fcdf2
3AME <45684631+3AME@users.noreply.github.com>
2022-08-08 16:45:52 +0300

    add muiEditor

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        ANFANG
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#view">View</a>
+        </li>
+        <li>
+          <a href="#sports">Sports</a>
+        </li>
+        <li>
+          <a href="#life">Life Skills</a>
+        </li>
+        <li>
+          <a href="#thoughts">Thoughts</a>
+        </li>
+      </ul>
+      <div className="footer__copyright">
+        <small>&copy; ANFANG. All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/thoughts/MuiEditor.js
+++ src/components/thoughts/MuiEditor.js
@@ -1,28 +1,43 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import MUIRichTextEditor from "mui-rte";
-import {red} from '@mui/material/colors';
 
 function MuiEditor() {
-  const save = (data) => {
-    console.log(data);
+  const save = (props) => {
+    console.log(props);
+    let textData = props;
+    console.log(textData.blocks);
+    console.log(typeof textData);
   };
   const myTheme = createTheme({
-    palette:{
-        primary:{
-            main:'#fffff',
-        }
-    }
+    palette: {
+      primary: {
+        main: "#fffff",
+      },
+    },
   });
   return (
-    <div className="container" style={{ display: "flex", textAlign:"center", justifyContent:"space-around"}}>
-      {/* <h2>I am a editor!</h2> */}
-      <ThemeProvider theme={myTheme}>
-        <MUIRichTextEditor
-          label="Type something here..."
-          onSave={save}
-          inlineToolbar={true}
-        ></MUIRichTextEditor>
-      </ThemeProvider>
+    <div>
+      <div
+        className="container"
+        style={{
+          display: "flex",
+          textAlign: "center",
+          justifyContent: "space-around",
+          width: 800,
+          height: 500,
+          backgroundColor: "pink",
+        }}
+      >
+        <ThemeProvider theme={myTheme}>
+          <MUIRichTextEditor
+            label="Type something here..."
+            onSave={save}
+            inlineToolbar={true}
+          ></MUIRichTextEditor>
+        </ThemeProvider>
+      </div>
+      <div>
+      </div>
     </div>
   );
 }
